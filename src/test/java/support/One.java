30e9f6da4b3a7017c8918cb3df047248da538990
gandalbhausaheb <gandalbhausaheb@gmail.com>
2022-01-25 23:32:37 +0530

    By Bhausaheb

--- src/test/java/support/One.java
+++ src/test/java/support/One.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 
+
 public class One 
 {
 	public static WebDriver driver=null;
@@ -26,7 +27,7 @@ public class One
 		{
 			System.setProperty("webdriver.chrome.driver", webDriverExePath);
 			driver=new ChromeDriver();
-			driver.manage().window().maximize();
+			driver.manage().window().maximize(); 
 		}
 		else if(strBrowserName.equalsIgnoreCase("firFox"))
 		{
@@ -51,16 +52,16 @@ public class One
 			try {
 			String strUrl=(String) inputparameters[0];
 			driver.get(strUrl);
-			 Outputparameters.put("STATUS", "PASS");
+			 Outputparameters.put("STATUS", "PASS"); 
 			 Outputparameters.put("MESSAGE", "methodused:OpenApplication, Input_Data :" + inputparameters[0].toString());
 			
 			}catch(Exception e)
 			{
-				Outputparameters.put("STATUS", "FAIL");
+				Outputparameters.put("STATUS", "FAIL"); 
 				 Outputparameters.put("MESSAGE", "methodused:OpenApplication, Input_Data :" + inputparameters[0].toString());
-				
+			
 			}
-			return Outputparameters;
+			return Outputparameters; 
 		}
 		
 		//click on cancel
@@ -68,7 +69,7 @@ public class One
 		{
 			try {
 			String xpath=(String) inputparameters[0];
-			driver.findElement(By.xpath(xpath)).click();
+			driver.findElement(By.xpath(xpath)).click(); 
 			 Outputparameters.put("STATUS", "PASS");
 			 Outputparameters.put("MESSAGE", "methodused:clickonElement, Input_Data :" + inputparameters[0].toString());
 			}catch(Exception e)
@@ -105,7 +106,7 @@ public class One
 			String xpath1=(String) inputparameters[1];
 			WebElement login=driver.findElement(By.xpath(xpath));
 					   login.sendKeys(xpath1);
-					   Outputparameters.put("STATUS", "PASS");
+					   Outputparameters.put("STATUS", "PASS"); 
 						 Outputparameters.put("MESSAGE", "methodused:LoginId, Input_Data :" + inputparameters[1].toString());
 			}catch(Exception e)
 			{
